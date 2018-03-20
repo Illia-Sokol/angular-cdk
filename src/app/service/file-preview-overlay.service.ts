3f12dcad106f07c73424bbd535330e1e398e1226
Illia Sokolovskyi <iljyas@compatibl.com>
2018-03-20 18:41:19 +0200

    first step to set sharing data with the overlay component

--- src/app/service/file-preview-overlay.service.ts
+++ src/app/service/file-preview-overlay.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
-import { ComponentPortal } from '@angular/cdk/portal';
+import { ComponentPortal, PortalInjector } from '@angular/cdk/portal';
 import { FilePreviewOverlayComponent } from '../file-preview/file-preview-overlay.component';
 import { FilePreviewOverlayConfig } from './file-preview-overlay.interface';
-import { FilerPreviewOverlayRef } from './file-preview-overlay-ref';
+import { FilePreviewOverlayRef } from './file-preview-overlay-ref';
 
 const DEFAULT_CONFIG: FilePreviewOverlayConfig = {
     panelClass: 'sokol',
@@ -25,12 +25,16 @@ export class FilePreviewOverlayService {
 
         const dialogConfig = { ...DEFAULT_CONFIG, ...config };
         const overlayRef = this.createOverlay(dialogConfig);
-        const dialogRef = new FilerPreviewOverlayRef(overlayRef);
+        const dialogRef = new FilePreviewOverlayRef(overlayRef);
         const filePreviewPortal = new ComponentPortal(FilePreviewOverlayComponent);
         overlayRef.attach(filePreviewPortal);
         overlayRef.backdropClick().subscribe( _ => dialogRef.close());
 
         return dialogRef;
+    }
+
+    private createIjector(config: FilePreviewDialogConfig, dialog: FilePreviewOverlayRef): PortalInjector {
+
     }
 
     private getOverlayConfig(config: FilePreviewOverlayConfig = {}): OverlayConfig {
