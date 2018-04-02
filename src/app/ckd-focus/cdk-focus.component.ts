9812364824d0114ed973efbebb6186742bfaa18f
Illia Sokolovskyi <iljyas@compatibl.com>
2018-04-02 18:47:39 +0300

    added focus branch

---
+++ src/app/ckd-focus/cdk-focus.component.ts
@@ -0,0 +1,13 @@
+// https://codeburst.io/angular-cdk-getting-started-accessibility-a11y-1b6143b961c
+
+// http://www.aisoftwarellc.com/blog/post/angular-component-development-kit-(cdk)/2057
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cdk-focus',
+    template: 'div'
+})
+
+export class CdkFocusComponent {
+}
